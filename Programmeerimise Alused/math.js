c631420aa23961c1ca5fd17bdd1b265f4f58e048
Andreas Kirs <andreas.kirs@voco.ee>
2023-09-19 14:02:28 +0300

    homework ymardamine

---
+++ Programmeerimise Alused/math.js
@@ -0,0 +1,20 @@
+let number1 = 100
+let number2 = 50
+let result
+
+// Nimi v6rdub v22rtus
+
+result = number1 + number2
+result = number1 - number2
+result = number1 * number2
+result = number1 / number2
+
+result = Math.PI
+result = Math.round(100.8)
+
+// Kodune ylesanne
+let value = 100.6 // ymarda allapoole kuidas?
+
+let value2 = 100.3 // ymarda yles
+
+console.log(result)
