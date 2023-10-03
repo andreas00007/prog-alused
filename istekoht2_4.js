990c1b926813a7cc926a9bde438f1998e4ea4d51
Andreas Kirs <andreas.kirs@voco.ee>
2023-10-03 15:40:42 +0300

    Teen ylesandeid

--- istekoht2_4.js
+++ istekoht2_4.js
@@ -5,17 +5,24 @@ const readline = require('readline').createInterface({
 
 readline.question('Kas soovite istekoha valida ("ise") või loosida ("loos")? ', istekohaValik => {
 	readline.question('Kas soovite istuda akna ääres ("aken") või mitte ("muu")? ', asukoht => {
-			
+			let aken
+			let vahekäik
 			if (istekohaValik == "ise") {
-					readline.question('vali istekoht')
-			if (istekohaValik == "muu"){
-				console.log('test')
-			}	
+					readline.question('vali istekoht ("aken") või ("muu")')
+				if (asukoht == "aken"){
+					console.log('Valisite ise istekoha: ' + asukoht)}
+					else {
+					asukoht = 'vahekäigukoht'
+					console.log('Teie istekoht on ' + asukoht)
+					}
+				}
+			if (istekohaValik == "loos"){
+				console.log('loosiTest')
+			}
 			
 
 
 
 			readline.close();
-		}
 		})
-});
+		});
